54afaa12ac87e509a8cce3568dbdd7c0d9627a21
Samuel Marvin <samuelmarvin@Samuels-MacBook-Pro.local>
2022-03-11 15:28:58 -0600

    fix db

--- server.js
+++ server.js
@@ -34,19 +34,19 @@ app.post('/family', (req, res) => {
     .catch((err)=> res.sendStatus(500))
 })
 
-app.patch('/family/:id', (req, res) => {
-    const {age, name} = req.body;
-    const id = req.params.id;
-    const query = `
-    UPDATE family SET
-        age = COALESCE($1, age),
-        name = COALESCE($2, name)
-    WHERE id = $3
-    RETURNING *`;
-    pool.query(query, [age, name, id])
-    .then((result) => res.send(result.rows))
-    .catch( (err) => res.sendStatus(500))
-});
+// app.patch('/family/:id', (req, res) => {
+//     const {age, name} = req.body;
+//     const id = req.params.id;
+//     const query = `
+//     UPDATE family SET
+//         age = COALESCE($1, age),
+//         name = COALESCE($2, name)
+//     WHERE id = $3
+//     RETURNING *`;
+//     pool.query(query, [age, name, id])
+//     .then((result) => res.send(result.rows))
+//     .catch( (err) => res.sendStatus(500))
+// });
 
 app.delete('/family/:id', (req,res)=>{
     const id = req.params.id;
